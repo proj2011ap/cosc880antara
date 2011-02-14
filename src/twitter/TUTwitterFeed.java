343c85904db572e5245ff47099a424a57d7c27e8
myselfantara@gmail.com <myselfantara@gmail.com@28073451-dfd6-355b-bb15-1cfa3c1127fd>
2011-02-14 21:33:09 +0000

    version 1

---
+++ src/twitter/TUTwitterFeed.java
@@ -0,0 +1,15 @@
+package twitter;
+
+public class TUTwitterFeed {
+	private String userName;
+		
+	public TUTwitterFeed(String userTU){
+		this.userName = userTU;		
+	}
+	
+	public String getUser(){
+		return this.userName;
+	}	
+	
+}
+
