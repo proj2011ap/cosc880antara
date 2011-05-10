977ba3d91025cbbd8cfcee8af25af4f6a4ff7457
jdehlinger@gmail.com <jdehlinger@gmail.com@28073451-dfd6-355b-bb15-1cfa3c1127fd>
2011-05-10 04:26:21 +0000

    Formatted date

--- Feb21TUTweet/src/twitter/DB.java
+++ Feb21TUTweet/src/twitter/DB.java
@@ -3,16 +3,17 @@ package twitter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.sql.Date;
+import java.util.Date;
+import java.util.Calendar;
 
 import twitter4j.Status;
 
 public class DB {
-	
+
 	Connection conn;
-	
+
 	public DB() {}
-	
+
 	public void dbConnect(String dbName, String username, String pw)
 	{
 		try
@@ -26,25 +27,28 @@ public class DB {
 			e.printStackTrace();
 		}
 	}
-	
-	public String prepareDate(Date d){		
-		
-	/*	java.util.Date dj = new java.util.Date();
-	    java.sql.Date sqlDate = new java.sql.Date(dj.getTime());
-	    System.out.println("utilDate:" + dj);
-	    System.out.println("sqlDate:" + sqlDate);    
-        s.equals(sqlDate); */
-		String s =" ";
-		s = d.getYear()+ " - "+ d.getMonth() +" - "+ d.getDay() + d.getHours() +" : "+ d.getMinutes()+ 
-				" : "+ d.getSeconds();
-		System.out.println(s);
+
+	public String prepareDate(Date d){
+
+		Calendar c = Calendar.getInstance();
+		c.setTime(d);
+
+		String s = "";
+		s = Integer.toString(c.get(c.YEAR)) + "-" + Integer.toString(c.get(c.MONTH)) + "-"
+			+ Integer.toString(c.get(c.DAY_OF_MONTH)) + " " + Integer.toString(c.get(c.HOUR_OF_DAY)) + ":"
+			+ Integer.toString(c.get(c.MINUTE)) + ":" + Integer.toString(c.get(c.SECOND)) + "."
+			+ Integer.toString(c.get(c.MILLISECOND));
+
+
+		System.out.println("HERE: " + s);
+
 		return s;
 	}
-	
+
 	public void insertData(String username, Date date, String text, String address)
 	{
 		String dateString = prepareDate(date);
-		
+
 	    Statement stmt;
 	    try
 	    {
@@ -53,18 +57,18 @@ public class DB {
 	       // text = text.replaceAll("'", " ");
 	        int id = 0;
 	        String cmd = "insert into TUTwitter values(NULL, '" + username + "', '" + dateString + "', '" + text + "', '" + address + "');";
-	        
+
 	        System.out.println(cmd);
 	        stmt.setEscapeProcessing(true);
-	        
+
 	        stmt.executeUpdate(cmd);
 	        stmt.close();
-	        
+
 	    }
 	    catch (Exception e)
 	    {
 	        e.printStackTrace();
-	        
+
 	    }
 	}
 }
